ec0719cfa533d466905876fbdb02a97174419413
Juan Diego Rodriguez <jrodriguez@ices.utexas.edu>
2018-09-23 18:08:35 -0500

    Minor correction.

--- data/BBN/clean.sh
+++ data/BBN/clean.sh
@@ -2,9 +2,9 @@
 sed -i '194s/NUMEX/TIMEX/' original_BBN_dataset/data/WSJtypes-subtypes/wsj00c.qa
 
 # Fix ampersand issue: & -> %amp;
-sed -i 's/&amp;/TEMP/g' original_BBN_dataset/data/WSJtypes-subtypes/*
+sed -i 's/&amp;/TTEMP/g' original_BBN_dataset/data/WSJtypes-subtypes/*
 sed -i 's/&/&amp;/g' original_BBN_dataset/data/WSJtypes-subtypes/*
-sed -i 's/TEMP/\&amp;/g' original_BBN_dataset/data/WSJtypes-subtypes/*
+sed -i 's/TTEMP/\&amp;/g' original_BBN_dataset/data/WSJtypes-subtypes/*
 
 # Fix broken or missing  <ROOT> and </ROOT>:
 python rootadd.py
